543b215e0fa3d454c854d1a5911ec893ef33ae72
AntonisGargalis <antonis7gar@gmail.com>
2024-04-18 18:14:06 +0300

    Add project files.

---
+++ VideoGamesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VideoGamesApp.Data
+{
+    public class ApplicationDbContext : DbContext // the class inherited everything from package
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
---
+++ VideoGamesApp/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace VideoGamesApp.Models
+{
+    public class Category
+    {
+        [Key] // data annotation primary key for id
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
