99d42d57d93825803f2403c617a5d30bc6a57476
AntonisGargalis <antonis7gar@gmail.com>
2024-05-13 18:57:44 +0300

    Create IUnitOfWork  and define routing by Areas

--- VideoGamesApp/Areas/Admin/Controllers/CategoryController.cs
+++ VideoGamesApp/Areas/Admin/Controllers/CategoryController.cs
@@ -3,18 +3,19 @@ using VideoGames.DataAccess.Data;
 using VideoGames.DataAccess.Repository.IRepository;
 using VideoGames.Models;
 
-namespace VideoGamesApp.Controllers
+namespace VideoGamesApp.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
-        private readonly ICategoryRepository _categoryRepo;
-        public CategoryController(ICategoryRepository db) 
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
         {
-            _categoryRepo = db;
+            _unitOfWork = unitOfWork;
         }
         public IActionResult Index()
         {
-            List<Category>  objCategoryList = _categoryRepo.GetAll().ToList();
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
             return View(objCategoryList);
         }
         public IActionResult Create()
@@ -26,8 +27,8 @@ namespace VideoGamesApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                _categoryRepo.Add(obj); // add the object of new category to the database
-                _categoryRepo.Save();       // save and update the changes
+                _unitOfWork.Category.Add(obj); // add the object of new category to the database
+                _unitOfWork.Save();       // save and update the changes
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");  // redirect to category list
             }
@@ -36,11 +37,11 @@ namespace VideoGamesApp.Controllers
 
         public IActionResult Edit(int? id)
         {
-            if (id == null || id==0)
+            if (id == null || id == 0)
             {
-               return NotFound();
+                return NotFound();
             }
-            Category? categoryFromDv = _categoryRepo.Get(u => u.Id == id);
+            Category? categoryFromDv = _unitOfWork.Category.Get(u => u.Id == id);
             //Category? categoryFromDv1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
             //Category? categoryFromDv2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
             if (categoryFromDv == null)
@@ -54,8 +55,8 @@ namespace VideoGamesApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                _categoryRepo.Update(obj); // add the object of new category to the database
-                _categoryRepo.Save();       // save and update the changes
+                _unitOfWork.Category.Update(obj); // add the object of new category to the database
+                _unitOfWork.Save();       // save and update the changes
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");  // redirect to category list
             }
@@ -68,7 +69,7 @@ namespace VideoGamesApp.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDv = _categoryRepo.Get(u => u.Id == id);
+            Category? categoryFromDv = _unitOfWork.Category.Get(u => u.Id == id);
             if (categoryFromDv == null)
             {
                 return NotFound();
@@ -78,13 +79,13 @@ namespace VideoGamesApp.Controllers
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePost(int? id)
         {
-            Category? obj = _categoryRepo.Get(u => u.Id == id);
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
             if (obj == null)
             {
                 return NotFound();
             }
-            _categoryRepo.Remove(obj);
-            _categoryRepo.Save();       // save and update the changes
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();       // save and update the changes
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");  // redirect to category list
         }
--- VideoGamesApp/Areas/Customer/Controllers/HomeController.cs
+++ VideoGamesApp/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using VideoGames.Models;
 
-namespace VideoGamesApp.Controllers
+namespace VideoGamesApp.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
