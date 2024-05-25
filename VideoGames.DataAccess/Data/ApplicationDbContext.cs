d13d642d6bd3266abd3038d2807203c2ef4c0150
AntonisGargalis <antonis7gar@gmail.com>
2024-05-25 22:03:49 +0300

    register an application user rather identity user

--- VideoGames.DataAccess/Data/ApplicationDbContext.cs
+++ VideoGames.DataAccess/Data/ApplicationDbContext.cs
@@ -12,10 +12,10 @@ namespace VideoGames.DataAccess.Data
         {
 
         }
-
+        // Adding Mapper
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
