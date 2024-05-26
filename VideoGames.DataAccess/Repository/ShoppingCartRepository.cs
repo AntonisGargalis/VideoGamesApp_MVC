def922eaecc95ccf587283d8940b174459ef03da
AntonisGargalis <antonis7gar@gmail.com>
2024-05-26 12:14:41 +0300

    add ApplicationUser Repository and ShoppingCart model

---
+++ VideoGames.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoGames.DataAccess.Data;
+using VideoGames.DataAccess.Repository.IRepository;
+using VideoGames.Models;
+
+namespace VideoGames.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.shoppingCarts.Update(obj);
+        }
+    }
+}
